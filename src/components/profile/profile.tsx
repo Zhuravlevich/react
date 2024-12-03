efcd9118262edaaa772643a46d2f8a762b2114d7
Илья <mega1gb@gmail.com>
2024-12-03 19:18:55 +0300

    Добавили компонент кнопка и профиль

---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+  return (
+    <img
+      src="https://ionoto.ru/upload/medialibrary/a1f/tcs61nk83dig738gik8qtkcx6ue7sgek.png"
+      alt="Профиль пользователя"
+    />
+  );
+};
+
+export default Profile;
