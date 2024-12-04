f7babf34200b0244135ece2404d8e0db8a4e7285
Илья <mega1gb@gmail.com>
2024-12-04 21:37:10 +0300

    card

--- src/components/Name/Name.tsx
+++ src/components/Name/Name.tsx
@@ -4,7 +4,7 @@ const Name = () => {
   return (
     <div className={classes.name}>
       <p className={classes.id}>1</p>
-      <p>Наручные часы мужские SKMEI 1251</p>
+      <p>Наручные часы мужские SKMEI 1251 </p>
     </div>
   );
 };
--- src/components/Price/Price.tsx
+++ src/components/Price/Price.tsx
@@ -4,7 +4,7 @@ const Price = () => {
   return (
     <div className={classes.price_row}>
       <p className={classes.price}>Цена: 8165</p>
-      <p className={classes.discount}>-90%</p>
+      <p className={classes.discount}>-90% </p>
       <p className={classes.rate}>
         Рейтинг: <span className={classes.rate_num}>4.7</span>
       </p>
